c213c385811e2a07efde7ed460804276edf3763e
mubarak7785 <mubarakjinabade07@gmail.com>
2023-02-28 19:15:42 +0530

    created add new task layout

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,35 @@
-export const Home=()=>{
-    return(
-            <div>
-                homee
-            </div>
-    )
-}
+import "./Home.css";
+
+export const Home = () => {
+  return (
+    <div>
+      <div className="main_Task">
+      <div>
+          <h2>ADD NEW TASK</h2>
+        </div>
+        <div>
+          <p>Task Name</p>
+          <input type="text" placeholder="Name" />
+        </div>
+        <div>
+          <p>Description</p>
+          <textarea name="Enter the Task" id="" cols="30" rows="5"></textarea>
+        </div>
+        <div>
+          <p>Date</p>
+          <input type="date" placeholder="Enter the task" />
+        </div>
+        <div>
+          <p>Status</p>
+          <select name="" id="">
+            <option value="pending">Pending</option>
+            <option value="completed">Completed</option>
+          </select>
+        </div>
+        <div>
+            <button>Add</button>
+        </div>
+      </div>
+    </div>
+  );
+};
